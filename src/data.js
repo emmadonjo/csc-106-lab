a7a0ae970bf513f5aeae76a988f717f3c7215512
emmadonjo <jo4christ48@yahoo.com>
2026-03-29 15:58:23 +0100

    landing page

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const PRODUCTS = [
+    {
+        name: "Marigold Female High Class Trouser",
+        price: 0,
+        tag: "bestseller",
+        category: "Trousers",
+        image: "assets/images/products/classy-pants.jpg",
+        description: "",
+    }
+]
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const displayProducts = (products) => {
+    for (let i = 0; i < products.length; i++) {
+        const productCard = document.createElement("article");
+    }
+}
